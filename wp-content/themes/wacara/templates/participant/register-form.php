9c01d6ed7c9078abcc685b17adb248c42cd99eb6
rendy44 <rendy.de.p@gmail.com>
2019-09-18 19:37:58 +0700

    Shorten long code

--- wp-content/themes/wacara/templates/participant/register-form.php
+++ wp-content/themes/wacara/templates/participant/register-form.php
@@ -16,18 +16,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="col-lg-8 col-md-10 mx-auto">
 				<form id="frm_register" method="post">
 					<?php
-					// Maybe render name field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'name' ); // phpcs:ignore
-					// Maybe render email field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'email' ); // phpcs:ignore
-					// Maybe render company field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'company' ); // phpcs:ignore
-					// Maybe render position field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'position' ); // phpcs:ignore
-					// Maybe render id number field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'id_number' ); // phpcs:ignore
-					// Maybe render phone field.
-					echo apply_filters( 'sk_input_field_event', $event_id, 'phone' ); // phpcs:ignore
+					$fields = [ 'name', 'email', 'company', 'position', 'id_number', 'phone' ];
+
+					foreach ( $fields as $field ) {
+						echo apply_filters( 'sk_input_field_event', $event_id, $field ); // phpcs:ignore
+					}
 
 					// Check whether use payment or not.
 					if ( $use_payment ) {
