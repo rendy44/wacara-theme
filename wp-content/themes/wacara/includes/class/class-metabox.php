a785c9e735633343878800715689fa134691fa78
rendy44 <rendy.de.p@gmail.com>
2019-09-22 01:11:18 +0700

    phpcs compliant

--- wp-content/themes/wacara/includes/class/class-metabox.php
+++ wp-content/themes/wacara/includes/class/class-metabox.php
@@ -170,7 +170,7 @@ if ( ! class_exists( 'Skeleton\Metabox' ) ) {
 								'type'        => 'text_datetime_timestamp',
 								'time_format' => Helper::get_time_format(),
 								'attributes'  => [
-									'data-conditional-id'    => $this->meta_prefix . 'single_day',
+									'data-conditional-id' => $this->meta_prefix . 'single_day',
 									'data-conditional-value' => 'off',
 								],
 							],
@@ -629,10 +629,10 @@ if ( ! class_exists( 'Skeleton\Metabox' ) ) {
 								'preview_size' => [ 100, 100 ],
 								'text'         => [
 									'add_upload_files_text' => __( 'Add Images', 'wacara' ),
-									'remove_image_text'     => __( 'Remove Images', 'wacara' ),
-									'file_text'             => __( 'Image:', 'wacara' ),
-									'file_download_text'    => __( 'Download', 'wacara' ),
-									'remove_text'           => __( 'Remove', 'wacara' ),
+									'remove_image_text'  => __( 'Remove Images', 'wacara' ),
+									'file_text'          => __( 'Image:', 'wacara' ),
+									'file_download_text' => __( 'Download', 'wacara' ),
+									'remove_text'        => __( 'Remove', 'wacara' ),
 								],
 							],
 							[
@@ -844,17 +844,6 @@ if ( ! class_exists( 'Skeleton\Metabox' ) ) {
 					'title'        => esc_html__( 'Stripe Options', 'wacara' ),
 					'object_types' => [ 'options-page' ],
 					'option_key'   => $this->meta_prefix . 'stripe_options',
-					// The option key and admin menu page slug.
-					// 'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
-					// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-					// 'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
-					// 'capability'      => 'manage_options', // Cap required to view options-page.
-					// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-					// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-					// 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
-					// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-					// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
-					// 'message_cb'      => 'yourprefix_options_page_message_callback',
 				]
 			);
 			$main_options->add_field(
