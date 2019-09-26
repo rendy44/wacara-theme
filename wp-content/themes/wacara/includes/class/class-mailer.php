f919a2d3fab126bbd2b8c930b232859fe00d8fd0
rendy44 <rendy.de.p@gmail.com>
2019-09-26 22:01:46 +0700

    Add email notification after success checkin

--- wp-content/themes/wacara/includes/class/class-mailer.php
+++ wp-content/themes/wacara/includes/class/class-mailer.php
@@ -44,13 +44,10 @@ if ( ! class_exists( 'Skeleton\Mailer' ) ) {
 		 */
 		private function __construct() {
 			// Send email after finishing registration.
-			add_action(
-				'wacara_after_finishing_registration',
-				[
-					$this,
-					'send_email_after_finishing_registration_callback',
-				]
-			);
+			add_action( 'wacara_after_finishing_registration', [ $this, 'send_email_after_registration' ], 10 );
+
+			// Send email after checking in.
+			add_action( 'wacara_after_participant_checkin', [ $this, 'send_email_after_checkin' ], 10 );
 		}
 
 		/**
@@ -87,21 +84,42 @@ if ( ! class_exists( 'Skeleton\Mailer' ) ) {
 		 *
 		 * @param string $participant_id participant id.
 		 */
-		public function send_email_after_finishing_registration_callback( $participant_id ) {
-			$participant_email        = Helper::get_post_meta( 'email', $participant_id );
-			$participant_name         = Helper::get_post_meta( 'name', $participant_id );
-			$participant_booking_code = Helper::get_post_meta( 'booking_code', $participant_id );
-			$participant_event        = Helper::get_post_meta( 'event_id', $participant_id );
-			$event_name               = get_the_title( $participant_event );
+		public function send_email_after_registration( $participant_id ) {
+
+			// Fetch participant detail.
+			$participant      = new Participant( $participant_id );
+			$participant_data = $participant->get_data();
+			$event_name       = get_the_title( $participant_data['event_id'] );
 
 			$site_name = get_bloginfo( 'name' );
 			/* translators: 1: the site name */
 			$email_subject = sprintf( __( 'Welcome To %s', 'wacara' ), $site_name );
 			/* translators: 1: participant name, 2: event name, 3: booking code*/
-			$email_content = sprintf( __( 'Hello %1$s, thank you for registering to %2$s. This is your booking code: %3$s', 'wacara' ), $participant_name, $event_name, $participant_booking_code );
+			$email_content = sprintf( __( 'Hello %1$s, thank you for registering to %2$s. This is your booking code: %3$s', 'wacara' ), $participant_data['name'], $event_name, $participant_data['booking_code'] );
 
 			// Send the email.
-			$this->send_email( $participant_email, $participant_name, $email_subject, $email_content );
+			$this->send_email( $participant_data['email'], $participant_data['name'], $email_subject, $email_content );
+		}
+
+		/**
+		 * Callback for sending email after checking in.
+		 *
+		 * @param string $participant_id participant id.
+		 */
+		public function send_email_after_checkin( $participant_id ) {
+
+			// Fetch participant detail.
+			$participant      = new Participant( $participant_id );
+			$participant_data = $participant->get_data();
+			$event_name       = get_the_title( $participant_data['event_id'] );
+
+			/* translators: 1: the event name */
+			$email_subject = sprintf( __( 'Thank you for checking in to %s', 'wacara' ), $event_name );
+			/* translators: 1: participant name, 2: event name*/
+			$email_content = sprintf( __( 'Hello %1$s, thank you for checking in to %2$s', 'wacara' ), $participant_data['name'], $event_name );
+
+			// Send the email.
+			$this->send_email( $participant_data['email'], $participant_data['name'], $email_subject, $email_content );
 		}
 	}
 }
