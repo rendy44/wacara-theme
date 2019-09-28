dd1e86811fb84a05336ecd3102803bc8d1ee2dc6
rendy44 <rendy.de.p@gmail.com>
2019-09-28 20:22:42 +0700

    Update gallery style

--- wp-content/themes/wacara/templates/event/gallery.php
+++ wp-content/themes/wacara/templates/event/gallery.php
@@ -10,16 +10,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 
-<section class="bg-light gallery py-0" id="gallery" data-aos="zoom-in">
+<section class="bg-light gallery py-0" id="galleries" data-aos="zoom-in">
 	<div class="container-wrap">
-		<div class="row no-gutters">
+		<div class="row no-gutters justify-content-center galleries">
 			<?php
 			foreach ( $gallery as $image_id => $image_url ) {
 				?>
 				<div class="col-lg-3 col-md-4 col-sm-6">
-					<a href="<?php echo esc_attr( $image_url ); ?>">
-						<img class="img-fluid" src="<?php echo esc_attr( wp_get_attachment_image_url( $image_id, 'wacara_gallery_thumbnail' ) ); ?>">
-					</a>
+					<a href="<?php echo esc_attr( $image_url ); ?>" style="background-image: url(<?php echo esc_attr( wp_get_attachment_image_url( $image_id, 'wacara_gallery_thumbnail' ) ); ?>)"><i class="fa fa-search"></i></a>
 				</div>
 				<?php
 			}
