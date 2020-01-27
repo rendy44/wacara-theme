f58033d1ff0cb3ff0c1a5469898ce17acd039373
rendy44 <rendy.de.p@gmail.com>
2020-01-27 09:57:22 +0700

    Add registrant content template

--- wp-content/plugins/wacara/templates/registrant-content.php
+++ wp-content/plugins/wacara/templates/registrant-content.php
@@ -16,6 +16,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Fetch registrant object.
 $registrant = new Registrant( get_the_ID() );
 
+// Fetch registration status.
+$reg_status = $registrant->get_registration_status();
+
 /**
  * Wacara before registrant masthead hook.
  *
@@ -33,6 +36,31 @@ do_action( 'wacara_registrant_masthead', $registrant );
 /**
  * Wacara after registrant masthead hook.
  *
- * @param Registrant the object of the current registrant.
+ * @param Registrant $registrant the object of the current registrant.
  */
 do_action( 'wacara_after_registrant_masthead', $registrant );
+
+/**
+ * Wacara before registrant content hook.
+ *
+ * @param Registrant $registrant the object of the current registrant.
+ */
+do_action( 'wacara_before_registrant_content', $registrant );
+
+/**
+ * Wacara registrant content hook.
+ *
+ * @param Registrant $registrant the object of the current registrant.
+ * @param string $reg_status the status of the registration.
+ *                               '' for fresh participant.
+ *                               'done' for participant who is ready to get the qrcode.
+ *                               also you can customize your own status to meet your needs.
+ */
+do_action( 'wacara_registrant_content', $registrant, $reg_status );
+
+/**
+ * Wacara after registrant content hook.
+ *
+ * @param Registrant $registrant the object of the current registrant.
+ */
+do_action( 'wacara_after_registrant_content', $registrant );
