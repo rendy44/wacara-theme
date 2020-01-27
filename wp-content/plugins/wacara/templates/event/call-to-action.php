3ab9957e6d23957e80888ed356550a7a2eb3d4b6
rendy44 <rendy.de.p@gmail.com>
2020-01-27 09:44:02 +0700

    Fix call to action anchor

--- wp-content/plugins/wacara/templates/event/call-to-action.php
+++ wp-content/plugins/wacara/templates/event/call-to-action.php
@@ -19,5 +19,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<span class="wcr-event-alert-content"><?php echo esc_html( $alert_content ); ?></span>
 </div>
 <div class="wcr-event-alert-block-wrapper wcr-event-alert-cta-wrapper">
-	<a class="wcr-event-alert-cta"><?php echo esc_html( $alert_button ); ?></a>
+	<a class="wcr-event-alert-cta" href="#wcr-section-pricing"><?php echo esc_html( $alert_button ); ?></a>
 </div>
