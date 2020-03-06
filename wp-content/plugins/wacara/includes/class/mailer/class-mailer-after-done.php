fd43c3e54938bdc83942c61634a1fdf7e4cd9096
rendy44 <rendy.de.p@gmail.com>
2020-03-07 00:25:34 +0700

    Add booking code in success registrant email

--- wp-content/plugins/wacara/includes/class/mailer/class-mailer-after-done.php
+++ wp-content/plugins/wacara/includes/class/mailer/class-mailer-after-done.php
@@ -33,7 +33,8 @@ if ( ! class_exists( 'Wacara\Mailer_After_Done' ) ) {
 			$mailer_id        = 'after-done';
 			$mailer_title     = __( 'After completing', 'wacara' );
 			$mailer_recipient = "{$registrant->get_registrant_name()} <{$registrant->get_registrant_email()}>";
-			$mailer_subject   = __( 'Congratulation, your seat is secured', 'wacara' );
+			/* translators: %1$s : name of the event, %2$s : id of the invoice */
+			$mailer_subject = sprintf( __( '%1$s - Booking Code For #%2$s', 'wacara' ), $registrant->get_event_name(), $registrant->get_invoice_id() );
 
 			// Construct the parent.
 			parent::__construct( $mailer_id, $mailer_title, true, $mailer_recipient, $mailer_subject, $registrant );
