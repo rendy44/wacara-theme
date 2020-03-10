770333b09f25ade2abef0099c548a99bd2b8d7ad
rendy44 <rendy.de.p@gmail.com>
2020-03-10 10:03:20 +0700

    Add simple footer

--- wp-content/themes/wacara-demo/footer-wacara.php
+++ wp-content/themes/wacara-demo/footer-wacara.php
@@ -12,5 +12,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 } ?>
 
 </body>
+<footer class="wcr-footer wcr-footer-dark">
+	<div class="frow-container">
+		<div class="frow">
+			<div class="col-sm-2-3 wcr-text-center">
+				<?php
+				/* translators: %1$s : site name, %2$s : site description, %3$s : current year */
+				echo sprintf( _x( '<p class="wcr-footer-content">&copy; %1$s - %2$s %3$s</p>', 'footer content', 'wacara' ), get_bloginfo( 'name' ), get_bloginfo( 'description' ), date( 'Y' ) ); // phpcs:ignore ?>
+			</div>
+		</div>
+	</div>
+</footer>
 <?php wp_footer(); ?>
 </html>
